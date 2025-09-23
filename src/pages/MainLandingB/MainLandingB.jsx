5e5ab311813cae555f843ea678c46e8f3c61ad60
Nikita <belka.nik02@gmail.com>
2025-09-23 15:38:50 +0300

    bot comment

--- src/pages/MainLandingB/MainLandingB.jsx
+++ src/pages/MainLandingB/MainLandingB.jsx
@@ -77,10 +77,10 @@ export default function MainLandingB() {
           <FcSms className='floatingButtonIcon' />
         )}
       </div> */}
-      <script
+      {/* <script
         src='https://cdn.pulse.is/livechat/loader.js'
         data-live-chat-id='68b9856b3e0f26e6880d9815'
-        async></script>
+        async></script> */}
     </div>
   );
 }
